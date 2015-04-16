6fbc15b2e611f43ab15493d233e1389a3ee7a1b2
A.Kramer <voitel@gmail.com>
2015-04-15 21:01:34 -0700

    created and tested empty package called sparser

---
+++ sparser/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sparser',
+      version='0.1',
+      description='URL scraper/parser',
+      url='https://github.com/voite1/Python300_final_project',
+      author='akramer',
+      license='GPL',
+	  packages=['sparser'],
+      install_requires=["BeautifulSoup4",],
+)
