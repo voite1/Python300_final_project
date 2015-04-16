0c7cc86420d00835600b7405a46c3c63a761c969
A.Kramer <voitel@gmail.com>
2015-04-15 21:07:48 -0700

    corrected spelling

--- sparser/setup.py
+++ sparser/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='sparser',
     version='0.1',
-    description='URL scraper/parser',
+    description='URL scraper/text extractor',
     url='https://github.com/voite1/Python300_final_project',
     author='akramer',
     license='GPL',
     packages=['sparser'],
-    install_requires=["BeautifulSoup4",],
+    install_requires=['BeautifulSoup4',],
 )
