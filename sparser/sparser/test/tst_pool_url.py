1ee871b891613675fc5873a29a5dcf626f791165
A.Kramer <xxxxx@gmail.com>
2015-05-06 19:49:55 -0700

    added multiprocessing to process pages

---
+++ sparser/sparser/test/tst_pool_url.py
@@ -0,0 +1,23 @@
+import multiprocessing as mp
+from sparser.urlsparser import parseurls
+from sparser.pagesparser import parsepages
+from sparser.pagesparser import __parsepage__
+from sparser.sutil import clean
+import pickle
+import datetime
+
+def test():
+    lst = parseurls("https://news.yahoo.com")
+
+
+    pool = mp.Pool(processes=20)
+    results = [pool.apply_async(__parsepage__, args=(x,)) for x in lst]
+    output = [p.get() for p in results]
+    print(len(output))
+    for i in output:
+    	print len(i)
+
+
+
+if __name__ == "__main__":
+    test()
---
+++ sparser/sparser/test/tst_single_url.py
@@ -0,0 +1,36 @@
+from sparser.urlsparser import parseurls
+from sparser.pagesparser import parsepages
+from sparser.sutil import clean
+import pickle
+import datetime
+
+if __name__ == "__main__":
+	lst = parseurls("https://news.google.com", 1)
+	print "URL's", len(lst)
+
+	with open("urls.txt", "wb") as output:
+		for i in lst:
+			output.write(i + "\n")
+
+	result = parsepages(lst, 50)
+	print "Pages", len(result)
+
+	my_dict = {}
+
+	for dict_in_list in result:
+		for i in dict_in_list:
+			if i in my_dict:
+				value = my_dict[i]
+				my_dict[i] = (value + dict_in_list[i])
+			else:
+				my_dict[i] = dict_in_list[i]
+
+	print "the", my_dict["the"]
+	print "over", my_dict["over"]
+	print "children", my_dict["children"]
+	print "wife", my_dict["wife"]
+	print "boeing", my_dict["boeing"]
+
+	filename = datetime.date.today().strftime("%m-%d-%Y")
+	filename = filename + ".dict.pickle"
+        pickle.dump(my_dict, open(filename, "wb"))
